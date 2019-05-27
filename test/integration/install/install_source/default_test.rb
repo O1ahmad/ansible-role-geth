5c81a5fc92d76f79ea03d95208318d63a5b0b6f9
Ahmad Wilson <ahmadwilson@pinterest.com>
2019-05-27 14:51:46 -0700

    remove user_setup dedicated integrated tests

--- test/integration/install/install_source/default_test.rb
+++ test/integration/install/install_source/default_test.rb
@@ -1,7 +1,17 @@
 title "Geth source installation default test suite"
 
+describe user('test') do
+  it { should exist }
+end
+
+describe group('test') do
+  it { should exist }
+end
+
 describe file('/opt/geth/geth') do
   it { should exist }
+  its('user') { should eq 'test' }
+  its('group') { should eq 'test' }
 end
 
 describe command('/opt/geth/geth version') do
--- test/integration/install/user_setup/default_test.rb
+++
@@ -1,7 +0,0 @@
-describe user("geth") do
-  it { should exist }
-end
-
-describe group("geth") do
-  it { should exist }
-end
